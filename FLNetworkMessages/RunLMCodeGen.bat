2ebcfa762dc96652717c9960b6461dcb092f5794
Arshia Ghafoori <arshia001@live.com>
2019-11-29 08:15:48 +0330

    Update path to client scripts (I really need to make these relative)

--- FLNetworkMessages/RunLMCodeGen.bat
+++ FLNetworkMessages/RunLMCodeGen.bat
@@ -1,1 +1,1 @@
-dotnet "..\..\LightMessage-Core\LightMessage.Compiler\bin\Release\netcoreapp2.1\LightMessage.Compiler.dll" messages.lm -simpl=..\FLGrains\CodeGen\Messages.g.cs -siface=..\FLGrainInterfaces\CodeGen\Messages.g.cs "-c=D:\Unity Projects\FLClient\Assets\Scripts\Network\ConnectionManager.cs"
+dotnet "..\..\LightMessage-Core\LightMessage.Compiler\bin\Release\netcoreapp2.1\LightMessage.Compiler.dll" messages.lm -simpl=..\FLGrains\CodeGen\Messages.g.cs -siface=..\FLGrainInterfaces\CodeGen\Messages.g.cs "-c=D:\UnityProjects\FLClient\Assets\Scripts\Network\ConnectionManager.cs"
