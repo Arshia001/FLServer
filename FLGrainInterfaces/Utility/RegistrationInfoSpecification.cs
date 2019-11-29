964276a00761957e99fe183f547e293e16362325
Arshia Ghafoori <arshia001@live.com>
2019-11-29 18:11:15 +0330

    Email and password validation rules

--- FLGrainInterfaces/Utility/RegistrationInfoSpecification.cs
+++ FLGrainInterfaces/Utility/RegistrationInfoSpecification.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace FLGrainInterfaces
 {
     public static class RegistrationInfoSpecification
     {
-        public static bool IsEmailAddressValid(string email) => true; //??
+        static Regex emailRegex = new Regex(@"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])");
 
-        public static bool IsPasswordComplexEnough(string password) => true; //??
+        public static bool IsEmailAddressValid(string email) => emailRegex.IsMatch(email);
+
+        public static bool IsPasswordComplexEnough(string password) => password.Length >= 6;
     }
 }
