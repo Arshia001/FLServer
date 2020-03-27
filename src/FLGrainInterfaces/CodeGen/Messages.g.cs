d3c32272f1f72e6826def7a1ccb65c4478e4c82e
Arshia Ghafoori <arshia001@live.com>
2020-03-27 12:59:53 +0430

    Replicate more config data to clients, to be used in tutorial and rule descriptions

--- src/FLGrainInterfaces/CodeGen/Messages.g.cs
+++ src/FLGrainInterfaces/CodeGen/Messages.g.cs
@@ -378,7 +378,7 @@ namespace FLGrainInterfaces
     [Orleans.Concurrency.Immutable]
     public class ConfigValuesDTO
     {
-        public ConfigValuesDTO(byte numRoundsToWinToGetReward, System.TimeSpan roundWinRewardInterval, uint numGoldRewardForWinningRounds, uint priceToRefreshGroups, System.TimeSpan roundTimeExtension, uint roundTimeExtensionPrice, uint revealWordPrice, uint getAnswersPrice, uint maxActiveGames, uint infinitePlayPrice, uint numTimeExtensionsPerRound, byte refreshGroupsAllowedPerRound)
+        public ConfigValuesDTO(byte numRoundsToWinToGetReward, System.TimeSpan roundWinRewardInterval, uint numGoldRewardForWinningRounds, uint priceToRefreshGroups, System.TimeSpan roundTimeExtension, uint roundTimeExtensionPrice, uint revealWordPrice, uint getAnswersPrice, uint maxActiveGames, uint infinitePlayPrice, uint numTimeExtensionsPerRound, byte refreshGroupsAllowedPerRound, System.TimeSpan infinitePlayTime, byte numRoundsPerGame, byte numGroupChoices, System.TimeSpan clientTimePerRound, System.TimeSpan gameInactivityTimeout, uint maxScoreGain, uint minScoreGain, float loserScoreLossRatio, uint winnerXPGain, uint loserXPGain, uint drawXPGain, uint winnerGoldGain, uint loserGoldGain, uint drawGoldGain)
         {
             this.NumRoundsToWinToGetReward = numRoundsToWinToGetReward;
             this.RoundWinRewardInterval = roundWinRewardInterval;
@@ -392,6 +392,20 @@ namespace FLGrainInterfaces
             this.InfinitePlayPrice = infinitePlayPrice;
             this.NumTimeExtensionsPerRound = numTimeExtensionsPerRound;
             this.RefreshGroupsAllowedPerRound = refreshGroupsAllowedPerRound;
+            this.InfinitePlayTime = infinitePlayTime;
+            this.NumRoundsPerGame = numRoundsPerGame;
+            this.NumGroupChoices = numGroupChoices;
+            this.ClientTimePerRound = clientTimePerRound;
+            this.GameInactivityTimeout = gameInactivityTimeout;
+            this.MaxScoreGain = maxScoreGain;
+            this.MinScoreGain = minScoreGain;
+            this.LoserScoreLossRatio = loserScoreLossRatio;
+            this.WinnerXPGain = winnerXPGain;
+            this.LoserXPGain = loserXPGain;
+            this.DrawXPGain = drawXPGain;
+            this.WinnerGoldGain = winnerGoldGain;
+            this.LoserGoldGain = loserGoldGain;
+            this.DrawGoldGain = drawGoldGain;
         }
 
         public byte NumRoundsToWinToGetReward { get; }
@@ -406,17 +420,31 @@ namespace FLGrainInterfaces
         public uint InfinitePlayPrice { get; }
         public uint NumTimeExtensionsPerRound { get; }
         public byte RefreshGroupsAllowedPerRound { get; }
+        public System.TimeSpan InfinitePlayTime { get; }
+        public byte NumRoundsPerGame { get; }
+        public byte NumGroupChoices { get; }
+        public System.TimeSpan ClientTimePerRound { get; }
+        public System.TimeSpan GameInactivityTimeout { get; }
+        public uint MaxScoreGain { get; }
+        public uint MinScoreGain { get; }
+        public float LoserScoreLossRatio { get; }
+        public uint WinnerXPGain { get; }
+        public uint LoserXPGain { get; }
+        public uint DrawXPGain { get; }
+        public uint WinnerGoldGain { get; }
+        public uint LoserGoldGain { get; }
+        public uint DrawGoldGain { get; }
 
-        public static implicit operator ConfigValuesDTO(FLGrainInterfaces.Configuration.ConfigValues obj) => new ConfigValuesDTO(obj.NumRoundsToWinToGetReward, obj.RoundWinRewardInterval, obj.NumGoldRewardForWinningRounds, obj.PriceToRefreshGroups, obj.RoundTimeExtension, obj.RoundTimeExtensionPrice, obj.RevealWordPrice, obj.GetAnswersPrice, obj.MaxActiveGames, obj.InfinitePlayPrice, obj.NumTimeExtensionsPerRound, obj.RefreshGroupsAllowedPerRound);
+        public static implicit operator ConfigValuesDTO(FLGrainInterfaces.Configuration.ConfigValues obj) => new ConfigValuesDTO(obj.NumRoundsToWinToGetReward, obj.RoundWinRewardInterval, obj.NumGoldRewardForWinningRounds, obj.PriceToRefreshGroups, obj.RoundTimeExtension, obj.RoundTimeExtensionPrice, obj.RevealWordPrice, obj.GetAnswersPrice, obj.MaxActiveGames, obj.InfinitePlayPrice, obj.NumTimeExtensionsPerRound, obj.RefreshGroupsAllowedPerRound, obj.InfinitePlayTime, obj.NumRoundsPerGame, obj.NumGroupChoices, obj.ClientTimePerRound, obj.GameInactivityTimeout, obj.MaxScoreGain, obj.MinScoreGain, obj.LoserScoreLossRatio, obj.WinnerXPGain, obj.LoserXPGain, obj.DrawXPGain, obj.WinnerGoldGain, obj.LoserGoldGain, obj.DrawGoldGain);
 
-        public LightMessage.Common.Messages.Param ToParam() => LightMessage.Common.Messages.Param.Array(LightMessage.Common.Messages.Param.UInt(NumRoundsToWinToGetReward), LightMessage.Common.Messages.Param.TimeSpan(RoundWinRewardInterval), LightMessage.Common.Messages.Param.UInt(NumGoldRewardForWinningRounds), LightMessage.Common.Messages.Param.UInt(PriceToRefreshGroups), LightMessage.Common.Messages.Param.TimeSpan(RoundTimeExtension), LightMessage.Common.Messages.Param.UInt(RoundTimeExtensionPrice), LightMessage.Common.Messages.Param.UInt(RevealWordPrice), LightMessage.Common.Messages.Param.UInt(GetAnswersPrice), LightMessage.Common.Messages.Param.UInt(MaxActiveGames), LightMessage.Common.Messages.Param.UInt(InfinitePlayPrice), LightMessage.Common.Messages.Param.UInt(NumTimeExtensionsPerRound), LightMessage.Common.Messages.Param.UInt(RefreshGroupsAllowedPerRound));
+        public LightMessage.Common.Messages.Param ToParam() => LightMessage.Common.Messages.Param.Array(LightMessage.Common.Messages.Param.UInt(NumRoundsToWinToGetReward), LightMessage.Common.Messages.Param.TimeSpan(RoundWinRewardInterval), LightMessage.Common.Messages.Param.UInt(NumGoldRewardForWinningRounds), LightMessage.Common.Messages.Param.UInt(PriceToRefreshGroups), LightMessage.Common.Messages.Param.TimeSpan(RoundTimeExtension), LightMessage.Common.Messages.Param.UInt(RoundTimeExtensionPrice), LightMessage.Common.Messages.Param.UInt(RevealWordPrice), LightMessage.Common.Messages.Param.UInt(GetAnswersPrice), LightMessage.Common.Messages.Param.UInt(MaxActiveGames), LightMessage.Common.Messages.Param.UInt(InfinitePlayPrice), LightMessage.Common.Messages.Param.UInt(NumTimeExtensionsPerRound), LightMessage.Common.Messages.Param.UInt(RefreshGroupsAllowedPerRound), LightMessage.Common.Messages.Param.TimeSpan(InfinitePlayTime), LightMessage.Common.Messages.Param.UInt(NumRoundsPerGame), LightMessage.Common.Messages.Param.UInt(NumGroupChoices), LightMessage.Common.Messages.Param.TimeSpan(ClientTimePerRound), LightMessage.Common.Messages.Param.TimeSpan(GameInactivityTimeout), LightMessage.Common.Messages.Param.UInt(MaxScoreGain), LightMessage.Common.Messages.Param.UInt(MinScoreGain), LightMessage.Common.Messages.Param.Float(LoserScoreLossRatio), LightMessage.Common.Messages.Param.UInt(WinnerXPGain), LightMessage.Common.Messages.Param.UInt(LoserXPGain), LightMessage.Common.Messages.Param.UInt(DrawXPGain), LightMessage.Common.Messages.Param.UInt(WinnerGoldGain), LightMessage.Common.Messages.Param.UInt(LoserGoldGain), LightMessage.Common.Messages.Param.UInt(DrawGoldGain));
 
         public static ConfigValuesDTO FromParam(LightMessage.Common.Messages.Param param)
         {
             if (param.IsNull)
                 return null;
             var array = param.AsArray;
-            return new ConfigValuesDTO((byte)array[0].AsUInt.Value, array[1].AsTimeSpan.Value, (uint)array[2].AsUInt.Value, (uint)array[3].AsUInt.Value, array[4].AsTimeSpan.Value, (uint)array[5].AsUInt.Value, (uint)array[6].AsUInt.Value, (uint)array[7].AsUInt.Value, (uint)array[8].AsUInt.Value, (uint)array[9].AsUInt.Value, (uint)array[10].AsUInt.Value, (byte)array[11].AsUInt.Value);
+            return new ConfigValuesDTO((byte)array[0].AsUInt.Value, array[1].AsTimeSpan.Value, (uint)array[2].AsUInt.Value, (uint)array[3].AsUInt.Value, array[4].AsTimeSpan.Value, (uint)array[5].AsUInt.Value, (uint)array[6].AsUInt.Value, (uint)array[7].AsUInt.Value, (uint)array[8].AsUInt.Value, (uint)array[9].AsUInt.Value, (uint)array[10].AsUInt.Value, (byte)array[11].AsUInt.Value, array[12].AsTimeSpan.Value, (byte)array[13].AsUInt.Value, (byte)array[14].AsUInt.Value, array[15].AsTimeSpan.Value, array[16].AsTimeSpan.Value, (uint)array[17].AsUInt.Value, (uint)array[18].AsUInt.Value, array[19].AsFloat.Value, (uint)array[20].AsUInt.Value, (uint)array[21].AsUInt.Value, (uint)array[22].AsUInt.Value, (uint)array[23].AsUInt.Value, (uint)array[24].AsUInt.Value, (uint)array[25].AsUInt.Value);
         }
     }
 
