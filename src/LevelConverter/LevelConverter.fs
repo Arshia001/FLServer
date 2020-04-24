bdddd983d4d41bf0d193087997a02603491916e1
Arshia001 <arshia001@live.com>
2020-04-24 16:11:03 +0430

    Remove obsolete level converter commands (gonna need to rename that utility too)

--- src/LevelConverter/LevelConverter.fs
+++ src/LevelConverter/LevelConverter.fs
@@ -13,84 +13,6 @@ type IncompleteCategory = string option * Word list
 let prepend a b = (a, b)
 let append b a = (a, b)
 
-let completeCategory (cat: IncompleteCategory) : Category =
-    match cat with
-    | (Some name, words) -> (name, words)
-    | _ -> failwith "Category without name"
-
-let addCategory catList incomplete = 
-    completeCategory incomplete :: catList
-
-let joinStrings sep (strs: string seq) = String.Join(sep, strs)
-
-let readCsv path =
-    let lines = File.ReadAllLines(path)
-
-    let empty = (List.empty<Category>, Option<IncompleteCategory>.None)
-
-    let (categoryList, last) = 
-        lines |>
-        Array.fold (fun (list, last) line -> 
-            if line.StartsWith('#') then
-                match last with
-                | Some x -> (addCategory list x, Some (None, List.empty))
-                | _ -> (list, Some (None, List.empty))
-            else
-                let split = line.Split([| ',' |], StringSplitOptions.RemoveEmptyEntries)
-                match (split, last) with
-                | [||], _ -> (list, last)
-                | _, Some (None, words) -> (list, Some (Some split.[0], words))
-                | _, Some (Some name, words) -> 
-                    match words |> List.tryFind (fun (name, _) -> name = split.[0]) with
-                    | Some _ ->
-                        Console.WriteLine("Duplicate word " + split.[0])
-                        (list, last)
-                    | _ ->
-                        let word = (split.[0], split |> Seq.skip 1 |> Seq.filter (fun s -> s <> split.[0]) |> List.ofSeq)
-                        (list, Some (Some name, word :: words))
-                | _, None -> failwith "Got words before category start"
-        ) empty
-
-    match last with
-    | Some x -> addCategory categoryList x
-    | _ -> categoryList
-
-let writeCategory (writer: StreamWriter) id (name, words) =
-    // a,1,"{('aa', {'ad', 'as'})}"
-    sprintf "%s,%i,\"{%s}\""
-        name
-        id
-        (words 
-            |> Seq.map (fun (main, corrections) -> 
-                sprintf "'%s': {%s}"
-                    main
-                    (corrections
-                        |> Seq.map (sprintf "'%s'")
-                        |> joinStrings ", "
-                    )
-            )
-            |> joinStrings ", "
-        )
-        |> writer.WriteLine
-
-let csvToCassandra inputPath outputPath =
-    use writer = new StreamWriter(outputPath, false, Encoding.UTF8)
-    readCsv inputPath |> Seq.iter (writeCategory writer 0)
-    
-let csvToFolder inputPath outputPath =
-    let categories = readCsv inputPath
-    Directory.CreateDirectory(outputPath) |> ignore
-    categories |> Seq.iter (fun (name, words) ->
-        use writer = new StreamWriter(Path.Combine(outputPath, name + ".txt"), false, Encoding.UTF8)
-        words |> Seq.iter (fun (main, corrections) ->
-            if corrections |> List.isEmpty then
-                sprintf "%s" main
-            else
-                sprintf "%s:%s" main (corrections |> joinStrings "|")
-            |> writer.WriteLine
-        )
-    )
-
 let persianNumbers = [| '۰'; '۱'; '۲'; '۳'; '۴'; '۵'; '۶'; '۷'; '۸'; '۹' |]
 let latinNumbers = [| '0'; '1'; '2'; '3'; '4'; '5'; '6'; '7'; '8'; '9' |]
 
@@ -193,22 +115,6 @@ let readGroup (directory: string) : Group =
     | _ -> 
         failwithf "Invalid directory name: %s" dirName
 
-let folderToCassandra inputPath outputPath =
-    if Directory.GetFiles(inputPath).Length > 0 then
-        printfn "There cannot be any files in the root of the groups directory"
-        Environment.Exit -1
-    else
-        Directory.CreateDirectory(outputPath) |> ignore
-        use groupsWriter = new StreamWriter(Path.Combine(outputPath, "groups.csv"))
-        use categoriesWriter = new StreamWriter(Path.Combine(outputPath, "categories.csv"))
-
-        Directory.GetDirectories inputPath
-        |> Seq.map readGroup
-        |> Seq.iter (fun (id, name, words) ->
-            sprintf "%i,%s" id name |> groupsWriter.WriteLine
-            words |> Seq.iter (writeCategory categoriesWriter id)
-        )
-
 type GroupCategoryData = JsonProvider<"../Misc/category-format.json">
 
 let folderToJson inputPath outputPath =
@@ -249,13 +155,7 @@ let folderToJson inputPath outputPath =
 
 [<EntryPoint>]
 let main argv =
-    let command = argv.[0]
-    let inputPath = argv.[1]
-    let outputPath = argv.[2]
-    match command with
-    | "csvToCassandra" -> csvToCassandra inputPath outputPath
-    | "csvToFolder" -> csvToFolder inputPath outputPath
-    | "folderToCassandra" -> folderToCassandra inputPath outputPath
-    | "folderToJson" -> folderToJson inputPath outputPath
-    | _ -> printfn "Unknown command, should be one of csvToCassandra, csvToFolder, folderToCassandra, folderToJson"
+    let inputPath = argv.[0]
+    let outputPath = argv.[1]
+    folderToJson inputPath outputPath
     0
