adfea4e2cb740d8996efa36ad73c45e378364e49
Arshia001 <arshia001@live.com>
2020-05-24 17:48:43 +0430

    Add analytics category data to push notifications (resolves #44 on server side)

--- src/FLGrains/Services/FcmNotificationService.cs
+++ src/FLGrains/Services/FcmNotificationService.cs
@@ -13,10 +13,9 @@ namespace FLGrains.Services
 {
     class FcmNotificationService : IFcmNotificationService
     {
-        private readonly IConfigReader configReader;
         private readonly ILogger<FcmNotificationService> logger;
 
-        public FcmNotificationService(ISystemSettingsProvider settingsProvider, IConfigReader configReader, ILogger<FcmNotificationService> logger)
+        public FcmNotificationService(ISystemSettingsProvider settingsProvider, ILogger<FcmNotificationService> logger)
         {
             var options = new AppOptions
             {
@@ -24,11 +23,10 @@ namespace FLGrains.Services
             };
             FirebaseApp.Create(options);
 
-            this.configReader = configReader;
             this.logger = logger;
         }
 
-        async void Send(string token, string title, string body, string collapseKey)
+        async void Send(string token, string title, string body, string collapseKey, string analyticsCategory)
         {
             try
             {
@@ -44,7 +42,11 @@ namespace FLGrains.Services
                         },
                         CollapseKey = collapseKey
                     },
-                    Token = token
+                    Token = token,
+                    Data = new Dictionary<string, string> 
+                    {
+                        { "analytics_category", analyticsCategory }
+                    }
                 };
                 await FirebaseMessaging.DefaultInstance.SendAsync(message);
             }
@@ -55,18 +57,18 @@ namespace FLGrains.Services
         }
 
         public void SendGameEnded(string token, string opponentName) =>
-            Send(token, "بازی تموم شد", $"بازیت با {opponentName} تموم شد. بیا نتیجه رو ببین!", "gameend");
+            Send(token, "بازی تموم شد", $"بازیت با {opponentName} تموم شد. بیا نتیجه رو ببین!", "gameend", "gameend");
 
         public void SendMyTurnStarted(string token, string opponentName) =>
-            Send(token, "نوبتت شروع شد", $"حریفت {opponentName} نوبتشو بازی کرد، نوبت تو شروع شده!", "turnend");
+            Send(token, "نوبتت شروع شد", $"حریفت {opponentName} نوبتشو بازی کرد، نوبت تو شروع شده!", "turnend", "turnend");
 
         public void SendDay4Reminder(string token) =>
-            Send(token, "خیلی وقته بهمون سر نزدی!", "همه دارن سر امتیاز با هم می‌جنگن، تو هم بیا رتبه‌ت رو ببر بالا که ازشون عقب نمونی!", "day4");
+            Send(token, "خیلی وقته بهمون سر نزدی!", "همه دارن سر امتیاز با هم می‌جنگن، تو هم بیا رتبه‌ت رو ببر بالا که ازشون عقب نمونی!", "day4", "day4");
 
         public void SendRoundWinRewardAvailableReminder(string token) =>
-            Send(token, "چالش روزانه آماده شد", "جعبه چالش روزانه آماده شده. بیا بازی کن و جایزه‌ت رو بگیر!", "roundwinreward");
+            Send(token, "چالش روزانه آماده شد", "جعبه چالش روزانه آماده شده. بیا بازی کن و جایزه‌ت رو بگیر!", "roundwinreward", "roundwinreward");
 
         public void SendCoinRewardVideoReadyReminder(string token) =>
-            Send(token, "هدیه آماده شد", "هدیه‌ت آماده شده، بیا تو بازی تا دریافتش کنی!", "coinrewardvideo");
+            Send(token, "هدیه آماده شد", "هدیه‌ت آماده شده، بیا تو بازی تا دریافتش کنی!", "coinrewardvideo", "coinrewardvideo");
     }
 }
